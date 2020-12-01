64932cbd5d1019e2b8a3cd740b373acd5e49e32d
Steven Balan <srbalan@outlook.ph>
2020-12-01 20:53:14 +0800

    Added Hero model

---
+++ MarvelHeroes/Models/Hero.swift
@@ -0,0 +1,19 @@
+//
+//  Hero.swift
+//  MarvelHeroes
+//
+//  Created by Lemi Developer on 12/1/20.
+//
+
+import Foundation
+
+struct Hero: Codable {
+    let name: String
+    let realname: String
+    let team: String
+    let firstappearance: String
+    let createdby: String
+    let publisher: String
+    let imageurl: String
+    let bio: String
+}
